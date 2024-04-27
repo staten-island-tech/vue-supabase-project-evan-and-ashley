a704b43af563d350bc46d7d51a608d484ea06eb2
Verviam <gaoevan37@gmail.com>
2024-04-27 13:33:00 -0400

    login?

--- bookstore-reviews/src/views/HomeView.vue
+++ bookstore-reviews/src/views/HomeView.vue
@@ -1,7 +1,44 @@
 <template>
-  <div></div>
+  <div>
+    <div>
+      <form name="login-form" @submit.prevent="login">
+        <div>
+          <label for="username">Username: </label>
+          <input id="username" type="text" v-model="username" />
+        </div>
+        <div>
+          <label for="password">Password: </label>
+          <input id="password" type="password" v-model="password" />
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const username = ref('')
+const password = ref('')
+
+async function login() {
+  try {
+    const response = await fetch('http://localhost:3000/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: username.value.toLowerCase(),
+        password: password.value
+      })
+    })
+    const data = await response.json()
+    console.log(data)
+  } catch (error) {
+    console.error(error)
+  }
+}
+</script>
 
 <style scoped></style>
