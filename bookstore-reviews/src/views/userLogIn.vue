630f7732229c8cc16699e6ba780a6d51fc9f32b4
ashleyyz068 <144456040+ashleyyz068@users.noreply.github.com>
2024-04-27 13:36:35 -0400

    nmn,mn

--- bookstore-reviews/src/views/userLogIn.vue
+++ bookstore-reviews/src/views/userLogIn.vue
@@ -4,10 +4,27 @@
     </div>
 </template>
 
-<script>
-    export default {
-        
+<script setup>
+import {useState} from "react"
+const Create = () => {
+    const [name, setTitle] = useState ('')
+    const [method, setMethod] = useState('')
+    const [rating,setRating] = useState ('')
+    const [formError, setFormError] = useState(null)
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        if (!name || !method || !rating ){
+            setFormError('Please fill in all the fields correctly')
+            return
+        }
     }
+
+    return (
+         
+    )
+}
 </script>
 
 <style lang="scss" scoped>
