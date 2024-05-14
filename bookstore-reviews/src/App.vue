23d87275b1dcaa0d3594834ab3eadc200edf7a9c
Evan Gao <gaoevan37@gmail.com>
2024-05-14 09:11:52 -0400

    edit to sync with table editor values

--- bookstore-reviews/src/App.vue
+++ bookstore-reviews/src/App.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-
 import { ref, onMounted } from 'vue'
 import { supabase } from './lib/supabaseClient'
+import Account from './components/Account.vue'
+import Auth from './components/Auth.vue'
 
 const profiles = ref([])
 
@@ -14,6 +15,17 @@ async function getProfiles() {
     profiles.value = data
   }
 }
+
+const session = ref()
+onMounted(() => {
+  supabase.auth.getSession().then(({ data }) => {
+    session.value = data.session
+  })
+
+  supabase.auth.onAuthStateChange((_, _session) => {
+    session.value = _session
+  })
+})
 
 onMounted(async () => {
   await getProfiles()
@@ -31,6 +43,10 @@ onMounted(async () => {
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/profile">Profile</RouterLink>
     </nav>
+  </div>
+  <div class="container" style="padding: 50px 0 100px 0">
+    <Account v-if="session" :session="session" />
+    <Auth v-else />
   </div>
   <RouterView />
 </template>
