d9f5707238cddfd9fa23c1b132260848b4919efa
ashleyyz068 <144456040+ashleyyz068@users.noreply.github.com>
2024-06-04 00:21:44 -0400

    can insert stuff!!

--- bookstore-reviews/src/views/BookData.vue
+++ bookstore-reviews/src/views/BookData.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
         <h1> book info</h1>
-          <h1  >{{ works.title }}</h1>
-         <p >{{ works.description }}</p> 
-          
+        <h1>{{ works.title }}</h1>
+        <p>{{ works.description }}</p>
+
         <h2> Leave Review for the book</h2>
         <label for="rating"> Rating: </label>
         <select id="rating" v-model="rating">
@@ -17,7 +17,7 @@
         <label for="comment">Comment: </label>
         <textarea id="comment" v-model="comment"></textarea>
 
-        <button type ="submit">Submit Review</button>
+        <button type="submit" @click="submitReview">Submit Review</button>
 
     </div>
 </template>
@@ -25,6 +25,7 @@
 <script setup>
 import { onMounted, ref, computed } from 'vue';
 import { useRoute } from 'vue-router';
+import { supabase } from '@/lib/supabaseClient'
 
 const rating = ref('')
 const comment = ref('')
@@ -34,11 +35,12 @@ const API = computed(() => `https://openlibrary.org/works/${route.params.id}.jso
 const route = useRoute();
 const errorMessage = ref(null)
 const loading = ref(true);
+const user = ref (''); 
 //no paramter herE?
 async function fetchData() {
     try {
-        
-       
+
+
         const res = await fetch(`https://openlibrary.org/works/${route.params.id}.json`)
         if (res.status >= 200 && res.status < 300) {
             works.value = await res.json()
@@ -53,12 +55,47 @@ async function fetchData() {
         console.log(error)
         errorMessage.value = 'An error occurred while making the API request.'
     }
-    
+
 }
 
-onMounted(async() => {
-   await  fetchData() //paramater here
-    
+async function submitReview() {
+    if (rating.value && comment.value) {
+        try {
+            const { data, error } = await supabase
+                .from('review')
+                .insert([
+                    {
+                        book: works.value.key,
+                        user: user.value,
+                        rating: parseInt(rating.value),
+                        comment: comment.value,
+                    }
+                ]);
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Review submitted successfully')
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    } else {
+        alert('Please fill in all fields')
+    }
+}
+
+async function getCurrentUser (){
+    const { user, error } = await supabase.auth.getUser(); 
+    if (error) {
+        console.log(error);
+        return null
+    }
+    return user; 
+}
+
+onMounted(async () => {
+    await fetchData() //paramater here
+    user.value = await getCurrentUser (); 
 })
 </script>
 
